9081f9e3860cfd296dfeb3171768072c0391cda1
Atul Tiwari <tiwariatul9526@gmail.com>
2021-12-23 17:49:20 +0530

    create schema for post method

---
+++ main.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+#create instance
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_Posts():
+    return {"data":"This is Your Posts"}
+
+
+@app.post("/createPosts")
+def create_Posts(new_posts: Post):
+    print(new_posts)
+    return {"data": "Your Post is created"}
